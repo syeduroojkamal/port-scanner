138fdfef207960c593a18ec91b19d1109f8a37d9
Syed_Urooj_Kamal <syeduroojkamal26@gmail.com>
2021-04-13 17:08:51 +0530

    scanning port 80

--- port_scanner.py
+++ port_scanner.py
@@ -1,3 +1,11 @@
 import socket
 from IPy import IP
 
+ipaddress = input('[+] Enter Target to scan: ')
+port = 80
+
+try:
+    sock = socket.socket()
+    sock.connect((ipaddress, port))
+except:
+    print('[-] port 80 is closed')
