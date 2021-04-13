21bc0853863f3637ce8af284e1f9cbd04287bb36
Syed_Urooj_Kamal <syeduroojkamal26@gmail.com>
2021-04-13 17:25:17 +0530

    comment

--- port_scanner.py
+++ port_scanner.py
@@ -1,11 +1,14 @@
 import socket
 from IPy import IP
 
-ipaddress = input('[+] Enter Target to scan: ')
+ipaddress = input('[+] Enter Target to Scan: ')
 port = 80
 
 try:
     sock = socket.socket()
     sock.connect((ipaddress, port))
+    print('[+] Port 80 is Open')
 except:
-    print('[-] port 80 is closed')
+    print('[-] Port 80 is Closed')
+    
+# ip of mozilla for testing 104.18.165.34
